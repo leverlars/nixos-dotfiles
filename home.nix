99b5a4c14b0b6fbdd257b66681b473fb0d070ae1
leverlars <oskarjo@pm.me>
2025-11-24 22:07:19 +0100

    Added gruvbox theme for Helix

--- home.nix
+++ home.nix
@@ -82,30 +82,31 @@ in
   #	systemd.enable = true;
   #};
 
-  #programs.helix = {
-  #  enable = true;
-  #  settings = {
-  #    theme = "tokyonight_transparent";
-  #    editor.cursor-shape = {
-  #      normal = "block";
-  #      insert = "bar";
-  #      select = "underline";
-  #    };
-  #  };
-  #  languages.language = [
-  #    {
-  #      name = "nix";
-  #      auto-format = true;
-  #      formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
-  #    }
-  #  ];
-  #  themes = {
-  #    tokyonight_transparent = {
-  #      "inherits" = "tokyonight";
-  #      "ui.background" = { };
-  #    };
-  #  };
- # };
+  programs.helix = {
+    enable = true;
+    package = pkgs.evil-helix;
+    settings = {
+      theme = "gruvbox_transparent";
+      editor.cursor-shape = {
+        normal = "block";
+        insert = "bar";
+        select = "underline";
+      };
+    };
+    languages.language = [
+      {
+        name = "nix";
+        auto-format = true;
+        formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+      }
+    ];
+    themes = {
+      gruvbox_transparent = {
+        "inherits" = "gruvbox";
+        "ui.background" = { };
+      };
+    };
+  };
 
   xdg.configFile = builtins.mapAttrs (name: subpath: {
     source = create_symlink "${dotfiles}/${subpath}";
